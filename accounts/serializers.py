b264bba03be0d917bb6df6e35ed66c3fffdd9264
amir <amir.moloki8558@gmail.com>
2024-11-25 15:34:49 +0330

    setup serializers for login and signup

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -17,3 +17,52 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ['id', 'user', 'date_of_birth', 'gender', 'location', 'website']
+
+
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password_confirm = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'password', 'password_confirm']
+
+    def validate_username(self, value):
+        """ چک می‌کنیم که یوزرنیم تکراری نباشد """
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError("This username is already taken.")
+        return value
+
+    def validate_email(self, value):
+        """ چک می‌کنیم که ایمیل تکراری نباشد """
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("This email is already taken.")
+        return value
+
+    def validate(self, data):
+        """ چک می‌کنیم که رمز عبور و تایید آن یکی باشند """
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError({"password": "Passwords must match."})
+        return data
+
+    def create(self, validated_data):
+        """ کاربر جدید را ایجاد می‌کنیم """
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)  # رمز عبور را هش می‌کنیم
+        user.save()
+        return user
+
+
+from rest_framework import serializers
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
