04b03e0baff46efcf646950625a82a89ce9d9613
amir <amir.moloki8558@gmail.com>
2024-12-14 15:31:37 +0330

    Add delete message functionality with notification to group

--- chats/consumers.py
+++ chats/consumers.py
@@ -10,42 +10,32 @@ from rest_framework.exceptions import AuthenticationFailed
 class ChatConsumer(AsyncWebsocketConsumer):
 
     async def connect(self):
-        # دریافت شناسه چت از URL
         self.chat_id = self.scope['url_route']['kwargs']['chat_id']
         self.group_name = f"chat_{self.chat_id}"
 
-        # تلاش برای استخراج توکن از query string
         token = self.get_token_from_query_string(self.scope)
 
-        # اگر توکن از query string پیدا نشد، تلاش برای دریافت توکن از هدر Authorization
         if not token:
             for header in self.scope.get('headers', []):
                 if header[0] == b'authorization':
-                    token = header[1].decode().split(' ')[1]  # Bearer <token>
+                    token = header[1].decode().split(' ')[1]
                     break
 
-        # اگر هیچ توکنی پیدا نشد، اتصال را می‌بندیم
         if not token:
             await self.close()
             return
 
         try:
-            # اعتبارسنجی توکن و استخراج کاربر
             user = await self.get_user_from_token(token)
 
             if user is None:
                 raise AuthenticationFailed("Invalid token")
 
-            # اضافه کردن کاربر به scope
             self.scope['user'] = user
 
-            # ورود به گروه چت
             await self.channel_layer.group_add(self.group_name, self.channel_name)
-
-            # تایید اتصال قبل از ارسال پیام‌ها
             await self.accept()
 
-            # ارسال پیام‌های موجود در چت به کاربر
             messages = await self.get_messages()
 
             for message in messages:
@@ -53,26 +43,28 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 sender_profile_picture = await self.get_sender_profile_picture(message)
                 sender_bio = await self.get_sender_bio(message)
 
-                action = 'send'
-
                 await self.send(text_data=json.dumps({
                     'message': message.content,
                     'sender': sender_name,
                     'sender_profile_picture': sender_profile_picture,
                     'sender_bio': sender_bio,
-                    'action': action,
+                    'timestamp': message.timestamp.isoformat(),
+                    'read_by': [user.username for user in await database_sync_to_async(list)(message.read_by.all())],
+                    'is_edited': message.is_edited,
+                    'is_deleted': message.is_deleted,
+                    'action': 'send',
                     'message_id': message.id,
-                    'file': None,  # فرض می‌کنیم این پیام‌ها فایل ندارند
+                    'file': None,
                 }))
 
         except AuthenticationFailed:
             await self.close()
 
     def get_token_from_query_string(self, scope):
-        """استخراج توکن از query string"""
         token = parse_qs(scope.get('query_string', b'').decode()).get('token', [None])[0]
         return token
 
+    # بخش مربوط به دریافت داده‌ها از WebSocket و ارسال رویدادها به گروه
     async def receive(self, text_data):
         data = json.loads(text_data)
         action = data.get('action')
@@ -80,14 +72,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
         if action == 'send':
             message_content = data.get('message')
             file = data.get('file')
-
             sender = self.scope['user']
+
+            message = await self.save_message(sender, message_content)
 
             sender_name = sender.username
             sender_profile_picture = sender.profile_picture.url if sender.profile_picture else None
             sender_bio = sender.bio if sender.bio else ""
-
-            message = await self.save_message(sender, message_content)
 
             if file:
                 attachment = await self.save_attachment(message, file)
@@ -99,7 +90,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
             else:
                 file_data = None
 
-            # ارسال پیام به گروه
+            # ارسال پیام به گروه به روز شده
             await self.channel_layer.group_send(
                 self.group_name,
                 {
@@ -108,9 +99,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     'sender': sender_name,
                     'sender_profile_picture': sender_profile_picture,
                     'sender_bio': sender_bio,
-                    'file': file_data,
+                    'timestamp': message.timestamp.isoformat(),
+                    'read_by': [user.username for user in await database_sync_to_async(list)(message.read_by.all())],
+                    'is_edited': message.is_edited,
+                    'is_deleted': message.is_deleted,
                     'action': 'send',
                     'message_id': message.id,
+                    'file': file_data,
                 }
             )
 
@@ -119,8 +114,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             new_content = data.get('new_message')
             sender = self.scope['user']
 
+            # ویرایش پیام
             message = await self.edit_message(message_id, new_content)
 
+            # ارسال پیام ویرایش شده به گروه
             await self.channel_layer.group_send(
                 self.group_name,
                 {
@@ -129,6 +126,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
                     'sender': sender.username,
                     'sender_profile_picture': sender.profile_picture.url if sender.profile_picture else None,
                     'sender_bio': sender.bio if sender.bio else "",
+                    'timestamp': message.timestamp.isoformat(),
+                    'read_by': [user.username for user in await database_sync_to_async(list)(message.read_by.all())],
+                    'is_edited': message.is_edited,
+                    'is_deleted': message.is_deleted,
                     'action': 'edit',
                     'message_id': message.id,
                 }
@@ -138,8 +139,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             message_id = data.get('message_id')
             sender = self.scope['user']
 
+            # حذف پیام
             await self.delete_message(message_id)
 
+            # ارسال پیام حذف شده به گروه
             await self.channel_layer.group_send(
                 self.group_name,
                 {
@@ -152,24 +155,60 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         elif action == 'read':
             message_id = data.get('message_id')
-
-            # فراخوانی متد mark_as_read
             await self.mark_as_read(message_id)
 
-            # ارسال پیام به گروه برای اطلاع‌رسانی سایر اعضا
+            # ارسال اطلاعات خوانده شده به گروه
             await self.channel_layer.group_send(
                 self.group_name,
                 {
                     'type': 'chat_message',
                     'message_id': message_id,
                     'action': 'read',
+                    'read_by': [user.username for user in
+                                await database_sync_to_async(list)((await self.get_message_read_by(message_id)))],
                     'sender': self.scope['user'].username,
                 }
             )
 
+    # مدیریت دریافت پیام‌ها در گروه (نمایش پیام‌ها)
+    async def chat_message(self, event):
+        message = event.get('message')
+        if not message:
+            return
+
+        sender_name = event.get('sender')
+        sender_profile_picture = event.get('sender_profile_picture')
+        sender_bio = event.get('sender_bio')
+        timestamp = event.get('timestamp')
+        read_by = event.get('read_by')
+        is_edited = event.get('is_edited')
+        is_deleted = event.get('is_deleted')
+        action = event.get('action')
+        message_id = event.get('message_id')
+        file_data = event.get('file')
+
+        # ارسال داده‌ها به WebSocket
+        await self.send(text_data=json.dumps({
+            'type': 'chat_message',
+            'message': message,
+            'sender': sender_name,
+            'sender_profile_picture': sender_profile_picture,
+            'sender_bio': sender_bio,
+            'timestamp': timestamp,
+            'read_by': read_by,
+            'is_edited': is_edited,
+            'is_deleted': is_deleted,
+            'action': action,
+            'message_id': message_id,
+            'file': file_data,
+        }))
+
     @database_sync_to_async
     def save_message(self, sender, content):
         message = Message.objects.create(sender=sender, content=content, chat_id=self.chat_id)
+        message.is_read = True
+        message.read_by.add(sender)
+        message.save()
         return message
 
     @database_sync_to_async
@@ -198,16 +237,14 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @database_sync_to_async
     def mark_as_read(self, message_id):
-        """علامت‌گذاری پیام به عنوان خوانده شده و اضافه کردن کاربر به لیست read_by"""
         message = Message.objects.get(id=message_id)
-        message.is_read = True
-        # اضافه کردن کاربر به لیست افرادی که پیام را خوانده‌اند
-        message.read_by.add(self.scope['user'])
-        message.save()
+        if self.scope['user'] not in message.read_by.all():
+            message.is_read = True
+            message.read_by.add(self.scope['user'])
+            message.save()
 
     @database_sync_to_async
     def get_messages(self):
-        # عملیات پایگاه داده که باید به صورت همزمان انجام شود
         return list(Message.objects.filter(chat=self.chat_id).order_by('timestamp'))
 
     @database_sync_to_async
@@ -220,33 +257,38 @@ class ChatConsumer(AsyncWebsocketConsumer):
         except (Exception, AuthenticationFailed):
             return None
 
+    @database_sync_to_async
+    def get_message_read_by(self, message_id):
+        message = Message.objects.get(id=message_id)
+        return message.read_by.all()
+
     async def chat_message(self, event):
-        message = event['message']
-        sender = event['sender']
-        sender_profile_picture = event['sender_profile_picture']
-        sender_bio = event['sender_bio']
-        action = event['action']
-        message_id = event['message_id']
+        message = event.get('message')
+        if not message:
+            return
+
+        sender_name = event.get('sender')
+        sender_profile_picture = event.get('sender_profile_picture')
+        sender_bio = event.get('sender_bio')
+        timestamp = event.get('timestamp')
+        read_by = event.get('read_by')
+        is_edited = event.get('is_edited')
+        is_deleted = event.get('is_deleted')
+        action = event.get('action')
+        message_id = event.get('message_id')
         file_data = event.get('file')
 
         await self.send(text_data=json.dumps({
+            'type': 'chat_message',
             'message': message,
-            'sender': sender,
+            'sender': sender_name,
             'sender_profile_picture': sender_profile_picture,
             'sender_bio': sender_bio,
+            'timestamp': timestamp,
+            'read_by': read_by,
+            'is_edited': is_edited,
+            'is_deleted': is_deleted,
             'action': action,
             'message_id': message_id,
             'file': file_data,
         }))
-
-    @database_sync_to_async
-    def get_sender_username(self, message):
-        return message.sender.username
-
-    @database_sync_to_async
-    def get_sender_profile_picture(self, message):
-        return message.sender.profile_picture.url if message.sender.profile_picture else None
-
-    @database_sync_to_async
-    def get_sender_bio(self, message):
-        return message.sender.bio if message.sender.bio else ""
