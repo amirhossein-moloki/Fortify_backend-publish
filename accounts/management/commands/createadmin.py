9b8e47babf17e2e70b9d772d6155d22b570d825e
amir <amir.moloki8558@gmail.com>
2025-01-25 11:34:21 +0330

    Make existing user 'amira' a superuser if not already

--- accounts/management/commands/createadmin.py
+++ accounts/management/commands/createadmin.py
@@ -2,15 +2,17 @@ from django.core.management.base import BaseCommand
 from accounts.models import User
 
 class Command(BaseCommand):
-    help = "Create a superuser if one does not already exist"
+    help = "Make the existing user 'amira' a superuser if they are not already."
 
     def handle(self, *args, **kwargs):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(
-                username="amira",
-                email="amir.moloki85@gmail.com",
-                password="h@rad140252"
-            )
-            self.stdout.write("Admin user created successfully!")
-        else:
-            self.stdout.write("Admin user already exists!")
+        try:
+            user = User.objects.get(username="amira")
+            if not user.is_superuser:
+                user.is_superuser = True
+                user.is_staff = True  # برای دسترسی به پنل مدیریت
+                user.save()
+                self.stdout.write("User 'amira' has been made a superuser!")
+            else:
+                self.stdout.write("User 'amira' is already a superuser.")
+        except User.DoesNotExist:
+            self.stdout.write("User 'amira' does not exist!")
